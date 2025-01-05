a19ef36bfaf492343ac8e53022d350f05a792b34
syedaaryan <syedaaryans@gmail.com>
2025-01-05 12:23:08 +0530

    character stats now displayed in character screen

--- src/main/UI.java
+++ src/main/UI.java
@@ -227,12 +227,96 @@ public class UI {
     public void drawCharacterScreen() {
 
         //CREATE A FRAME
-        final int frameX = gp.tileSize * 2;
+        final int frameX = gp.tileSize;
         final int frameY = gp.tileSize;
         final int frameWidth = gp.tileSize * 5;
         final int frameHeight = gp.tileSize * 10;
 
         drawSubWindow(frameX, frameY, frameWidth, frameHeight);
+
+        // TEXT
+        g2.setColor(Color.white);
+        g2.setFont(g2.getFont().deriveFont(32F));
+
+        int textX = frameX + 20;
+        int textY = frameY + gp.tileSize;
+
+        // lineHeight is to keep the text away from each other horizontally
+        final int lineHeight = 35;
+
+        // NAMES
+        g2.drawString("Level", textX, textY);
+        textY += lineHeight;
+
+        g2.drawString("Life", textX, textY);
+        textY += lineHeight;
+
+        g2.drawString("Strength", textX, textY);
+        textY += lineHeight;
+
+        g2.drawString("Dexterity", textX, textY);
+        textY += lineHeight;
+
+        g2.drawString("Attack", textX, textY);
+        textY += lineHeight;
+
+        g2.drawString("Defense", textX, textY);
+        textY += lineHeight;
+
+        g2.drawString("Exp", textX, textY);
+        textY += lineHeight;
+
+        g2.drawString("Next Level", textX, textY);
+        textY += lineHeight;
+
+        g2.drawString("Coin", textX, textY);
+        textY += lineHeight + 20;
+
+        g2.drawString("Weapon", textX, textY);
+        textY += lineHeight + 15;
+
+        g2.drawString("Shield", textX, textY);
+
+        // VALUES
+        int tailX = (frameX + frameWidth) - 30;
+        textY = frameY + gp.tileSize; // Resetting textY;
+
+        characterScreenValues(String.valueOf(gp.player.level), tailX, textY);
+        textY += lineHeight;
+
+        characterScreenValues(gp.player.life + "/" + gp.player.maxLife, tailX, textY);
+        textY += lineHeight;
+
+        characterScreenValues(String.valueOf(gp.player.strength), tailX, textY);
+        textY += lineHeight;
+
+        characterScreenValues(String.valueOf(gp.player.dexterity), tailX, textY);
+        textY += lineHeight;
+
+        characterScreenValues(String.valueOf(gp.player.attack), tailX, textY);
+        textY += lineHeight;
+
+        characterScreenValues(String.valueOf(gp.player.defense), tailX, textY);
+        textY += lineHeight;
+
+        characterScreenValues(String.valueOf(gp.player.exp), tailX, textY);
+        textY += lineHeight;
+
+        characterScreenValues(String.valueOf(gp.player.nextLevelExp), tailX, textY);
+        textY += lineHeight;
+
+        characterScreenValues(String.valueOf(gp.player.coin), tailX, textY);
+        textY += lineHeight;
+
+        // TO DISPLAY THE IMAGES OF WEAPON AND SHIELD
+        g2.drawImage(gp.player.currentWeapon.down1, tailX - gp.tileSize, textY - 14, null);
+        textY += gp.tileSize;
+
+        g2.drawImage(gp.player.currentShield.down1, tailX - gp.tileSize, textY - 14, null);
+
+        /*So basically what happens is first the names get rendered, i,e the level, life etc., once that's done
+         * we reset the textY value and move the alignment of the text towards the right and render tha value of
+         * level, life etc.*/
 
     }
 
@@ -257,6 +341,22 @@ public class UI {
         int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
 
         return gp.screenWidth / 2 - length / 2;
+
+    }
+
+    // A method which returns the right alignment for the character stats screen
+    public int getXForAlignToRight(String text, int tailX) {
+
+        int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+        return tailX - length;
+
+    }
+
+    // This will display the values in the character screen
+    public void characterScreenValues(String value, int tailX, int textY) {
+
+        int textX = getXForAlignToRight(value, tailX);
+        g2.drawString(value, textX, textY);
 
     }
 
